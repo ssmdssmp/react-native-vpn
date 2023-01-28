464a40261a52a1d6accec11bd8e269d3a54e729e
PAI <PAI@d.c>
2023-01-28 12:34:33 +0200

    fixed bug with share

--- components/Share.tsx
+++ components/Share.tsx
@@ -1,23 +1,26 @@
 import {Share} from 'react-native';
 
-const onShare = async () => {
-  try {
-    const result = await Share.share({
-      message:
-        'Использую бесплатный VPN, рекомендую: https://freevpnplanet.com/download',
-    });
-    if (result.action === Share.sharedAction) {
-      if (result.activityType) {
-        // shared with activity type of result.activityType
-      } else {
-        // shared
+const debounce = require('lodash.debounce');
+
+const onShare = debounce(async () => {
+    try {
+      
+      const result = await Share.share({
+        message:
+          'https://freevpnplanet.com/download',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
       }
-    } else if (result.action === Share.dismissedAction) {
-      // dismissed
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
+    } catch (error) {
+      console.log(error);
+    } 
+},450);
 
 export default onShare;
